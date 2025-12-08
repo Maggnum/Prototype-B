1dfcbd761805446ff9ace921fa8829721b58bdc8
StudentSKV101 <yonatan2804@gmail.com>
2025-12-08 18:29:52 +0200

    hotfix(employeesPage.tsx): false deconstraction

--- src/components/EmployeesPage/employeesPage.tsx
+++ src/components/EmployeesPage/employeesPage.tsx
@@ -6,12 +6,12 @@ import type { employee } from "./types";
 import { useLoaderData } from "react-router";
 
 const EmployeesPage: FC = () => {
-  const { employees } = useLoaderData();
+  const employees = useLoaderData();
 
   return (
     <Container sx={{ py: 4 }}>
       <Grid container spacing={4} justifyContent="center">
-        {employees.map((employee: employee) => (
+        {employees?.map((employee: employee) => (
           <EmployeeCard
             id={employee.id}
             firstName={employee.firstName}
