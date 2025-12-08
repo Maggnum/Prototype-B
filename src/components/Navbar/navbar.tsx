5ab1dacf4973e6b94d2a749fd71b3dd91394bd9b
StudentSKV101 <yonatan2804@gmail.com>
2025-12-08 15:28:27 +0200

    feat(navbar.css, navbar.tsx): add navbar

---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,43 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import AdsClickIcon from "@mui/icons-material/AdsClick";
+
+const navbarStyle = {
+  backgroundColor: "rgba(20, 20, 20, 0.9)",
+  backdropFilter: "blur(6px)",
+  borderBottom: "1px solid rgba(255, 255, 255, 0.08)",
+};
+
+const toolbarStyle = {
+  px: 3,
+  display: "flex",
+  alignItems: "center",
+  minHeight: "64px",
+};
+
+const iconStyle = { fontSize: 28, mr: 1, color: "grey.200" };
+
+const itemStyle = {
+  mx: 2,
+  textDecoration: "none",
+  color: "grey.100",
+  fontWeight: 500,
+  letterSpacing: "0.5px",
+  "&:hover": {
+    color: "grey.500",
+  },
+};
+
+export const Navbar = () => {
+  return (
+    <AppBar position="sticky" elevation={0} sx={navbarStyle}>
+      <Toolbar sx={toolbarStyle}>
+        <AdsClickIcon sx={iconStyle} />
+        <Typography variant="h6" component="a" href="#" sx={itemStyle}>
+          Home
+        </Typography>{" "}
+      </Toolbar>
+    </AppBar>
+  );
+};
