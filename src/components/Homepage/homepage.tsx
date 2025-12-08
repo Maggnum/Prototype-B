8077ff465ea172ce27bac8353b8667a3ad245e3d
StudentSKV101 <yonatan2804@gmail.com>
2025-12-08 15:27:45 +0200

    feat(homepage.css, homepage.tsx): add homepage

---
+++ src/components/Homepage/homepage.tsx
@@ -0,0 +1,13 @@
+import "./homepage.css";
+
+const Homepage = () => {
+  return (
+    <div id="homepage">
+      <img src="src/assets/Image.jpg" alt="image" />
+      <h1>Large Header</h1>
+      <p>This is a generic react website</p>
+    </div>
+  );
+};
+
+export default Homepage;
