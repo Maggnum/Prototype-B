bacb4c9f7ebb536af3ca57c2ab14cc22946866b0
StudentSKV101 <yonatan2804@gmail.com>
2025-12-09 14:54:25 +0200

    feat(client.ts, geoConverter.ts): add city info api

--- src/services/northwindClient.ts
+++ src/services/northwindClient.ts
@@ -11,3 +11,11 @@ export const fetchEmployees = async () => {
   const { data } = await api.get("/employees");
   return data;
 };
+
+export const geoApi = axios.create({
+  baseURL: "https://api.api-ninjas.com/v1/city",
+  headers: {
+    "Content-Type": "application/json",
+    "X-Api-Key": "65i/KhSGhz8f5qrRkK4wTg==6rMi0BQXKjMKEtk5",
+  },
+});
