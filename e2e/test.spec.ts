f19d4f79c0b23949575f328a33023fc4275bd692
StudentSKV101 <yonatan2804@gmail.com>
2025-12-10 13:02:48 +0200

    feat(test.spec.ts): e2e tests, test groups, parallel testing

--- e2e/test.spec.ts
+++ e2e/test.spec.ts
@@ -1,34 +1,54 @@
 import { test, expect } from "@playwright/test";
 
-test("test employee page and modal", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
-  await page.getByRole("link", { name: "Employees" }).click();
-  await page
-    .getByRole("button", { name: "Andrew Fuller Andrew Fuller" })
-    .click();
-  await expect(
-    page.getByText(
-      "Andrew FullerVice President, Sales📍 Tacoma, USA🎂 2/19/1952mm yes this is the"
-    )
-  ).toBeVisible();
+test.describe.configure({ mode: "parallel" });
+test.describe("home page", () => {
+  test("image has loaded", async ({ page }) => {
+    await page.goto("http://localhost:5173/");
+    await expect(page.getByRole("img", { name: "image" })).toBeVisible();
+  });
+
+  test("navbar", async ({ page }) => {
+    await page.goto("http://localhost:5173/");
+    await page.getByRole("link", { name: "Employees" }).click();
+    await expect.soft(page).toHaveURL("http://localhost:5173/employees");
+    await page.getByRole("link", { name: "Map" }).click();
+    await expect.soft(page).toHaveURL("http://localhost:5173/map");
+    await page.getByRole("link", { name: "Home" }).click();
+    await expect(page).toHaveURL("http://localhost:5173/");
+  });
 });
 
-test("test map and employee popup", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
-  await page.getByRole("link", { name: "Map" }).click();
-  for (let index = 0; index < 15; index++) {
-    await page
-      .locator("div")
-      .nth(2)
-      .hover({ position: { x: 100, y: 100 } });
-    await page.mouse.down();
-    await page
-      .locator("div")
-      .nth(2)
-      .hover({ position: { x: 500, y: 440 } });
-    await page.mouse.up();
-  }
+test.describe("employee page", () => {
+  test("all employee cards visible", async ({ page }) => {
+    await page.goto("http://localhost:5173/");
+    await page.pause();
+    await page.getByRole("link", { name: "Employees" }).click();
+    await expect(page.locator("div.MuiCard-root img[alt]")).toHaveCount(9);
+  });
 
-  await page.getByRole("button", { name: "Marker" }).nth(4).click();
-  await expect(page.getByText("Steven Buchanan, Michael")).toBeVisible();
+  test("employee modal", async ({ page }) => {
+    await page.goto("http://localhost:5173/");
+    await page.getByRole("link", { name: "Employees" }).click();
+    await page
+      .getByRole("button", { name: "Andrew Fuller Andrew Fuller" })
+      .click();
+    await expect(
+      page.getByText(
+        "Andrew FullerVice President, Sales📍 Tacoma, USA🎂 2/19/1952mm yes this is the"
+      )
+    ).toBeVisible();
+  });
+});
+
+test.describe("map page", () => {
+  test("employee marker and popup", async ({ page }) => {
+    await page.goto("http://localhost:5173/");
+    await page.getByRole("link", { name: "Map" }).click();
+    for (let index = 0; index < 20; index++) {
+      await page.getByRole("button", { name: "Zoom out" }).click();
+    }
+
+    await page.getByRole("button", { name: "Marker" }).nth(4).click();
+    await expect(page.getByText("Steven Buchanan, Michael")).toBeVisible();
+  });
 });
