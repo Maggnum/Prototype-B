784338fa56d43fe89b3a7e26c92eaea9ea3bb196
StudentSKV101 <yonatan2804@gmail.com>
2025-12-10 17:33:36 +0200

    fix(App.tsx, main.tsx, routes.ts, employeeCard.tsx,employeeCard.tsx): remove default export

--- src/components/EmployeesPage/employeesPage.tsx
+++ src/components/EmployeesPage/employeesPage.tsx
@@ -2,11 +2,11 @@ import type { FC } from "react";
 import { EmployeeCard } from "./employeeCard";
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
-import type { employee } from "./types";
-import { useLoaderData } from "react-router";
+import type { employee } from "../../modules/employee";
+import { useOutletContext } from "react-router";
 
 export const EmployeesPage: FC = () => {
-  const employees = useLoaderData();
+  const employees: employee[] = useOutletContext();
 
   return (
     <Container sx={{ py: 4 }}>
